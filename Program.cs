fb15fafac32cdc1db29a7163210b44da9c9049dc
Kirill <shadow986@mail.ru>
2023-02-28 00:08:00 +0300

    Задание 27

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// найти сумму цифр во введённом числе
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int dlina = (int)Math.Log10(number) + 1;
+int buf = dlina-1;
+int buf1 = 0;
+int sum = 0;
+for (int count = 0; count < dlina; count++)
+{
+    buf1 = (int)Math.Pow(10,buf);
+    sum = sum+number/buf1;
+    number = number - (number/buf1)*buf1;
+    buf--;
+}
+Console.WriteLine ("Сумма цифр числа = " + sum);
